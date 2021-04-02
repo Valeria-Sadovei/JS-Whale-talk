2b8bd7abea4ef107a698a40274563c9874f693d2
valeria <valeriadanila99@gmail.com>
2021-04-02 16:35:11 +0300

    JS Whale talk

---
+++ whale talk.js
@@ -0,0 +1,17 @@
+let input ="Hello there, my friend";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+for (let i = 0; i < input.length; i++) {
+  for(let j = 0; j < vowels.length; j++) {
+    if(input[i] === vowels[j]) {
+    if(input[i] === "e") {
+      resultArray.push('ee');
+    } else if(input[i] === "u"){
+      resultArray.push('uu');
+    } else {
+      resultArray.push(input[i]);
+    }
+  }
+  }
+}
+console.log(resultArray.join('').toUpperCase());
